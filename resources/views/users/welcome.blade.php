e14c32b72573a7d5811bcf68ad8ab27bc2fc273c
TroyHou <1009372153@qq.com>
2017-03-16 13:26:44 +0800

    new push

--- resources/views/users/welcome.blade.php
+++ resources/views/users/welcome.blade.php
@@ -4,14 +4,20 @@
     <div class="body_two">
         <h2>2017 VICTOR品牌大會</h2>
         <h3>暨秋冬新品發布會</h3>
-        <p>
-            尊敬的来宾：<br />
+        <p style="margin-left: -1.2rem;margin-top:0.44rem;">尊敬的来宾：</p>
+        <p style="margin-left: 0.1rem;"> 2017 VICTOR品牌大会暨秋冬新品</p>
+        <p style="margin-left: 0.169rem;"> 发布会欢迎您！感谢您你始终如一的</p>
+        <p style="margin-left: -1.24rem;"> 支持与付出。</p>
+        <p style="margin-left: 0.1rem;"> 2017，让我们继续精诚合作，相携</p>
+        <p style="margin-left: -0.5rem;">与共，共创“胜利”辉煌!</p>
+        <!--<p>
+            <p>尊敬的来宾：<br /></p>
             2017 VICTOR品牌大会暨秋冬新品<br/>
             发布会欢迎您！感谢您你始终如一的<br />
                               支持与付出。<br />
             2017，让我们继续精诚合作，相携<br />
             与共，共创“胜利”辉煌!
-        </p>
+        </p>-->
         <a href="{{route('login')}}">获取入场凭证</a>
     </div>
     </body>
