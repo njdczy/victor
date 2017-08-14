5ca2d90eb2a8b8594870b2a0583b7c2ab175f834
zhenggg <bianzhenggg@gmail.com>
2017-08-14 14:50:03 +0800

    enter

--- app/Admin/Controllers/EnterController.php
+++ app/Admin/Controllers/EnterController.php
@@ -149,8 +149,8 @@ EOT;
                 $box = new Box('统计', '
 <span>应到人数:</span><b id="should_vuser_count">'.$should_vuser_count.'</b>
 <span>实到人数:</span><b id="sign_vuser_count">'.$sign_vuser_count['sign_count'].'</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<span>应到客户家数:</span><b id="should_vcat_count">'.$should_vcat_count.'</b>
-<span>实到客户家数:</span><b id="sign_vcat_count">'.$sign_vcat_count.'</b>
+<span>应到部门:</span><b id="should_vcat_count">'.$should_vcat_count.'</b>
+<span>实到部门:</span><b id="sign_vcat_count">'.$sign_vcat_count.'</b>
 <span>实到经销商数:</span><b id="sign_company_count">'.$sign_company_count.'</b>'
                 );
                 $content->row($box->style('primary'));
